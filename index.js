9fd80a01ae6feb51b5ccb779372e126d9ca993f9
AllenHsing3 <allen.hsing@gmail.com>
2021-02-11 16:24:29 -0800

    WS running

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const clients = {};
 
 //Every User gets a unique ID
 const getUniqueID = () => {
-    const s4 = () => Math.floor((1 + Math.random()) * 0*10000).toString(16).subString(1)
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
     return s4() + s4() + '-' + s4()
 }
 
--- websocketclient/src/App.js
+++ websocketclient/src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { Fragment, useState } from "react";
+
+// Set up to listen on 8000
+const client = new W3CWebSocket("ws://127.0.0.1:8000");
 
 function App() {
+  const [user, setUser] = useState({
+    userName: "",
+    loggedIn: false,
+    messages: [{user:"ChatBot", msg:'Welcome to the chat!', type:'message'}],
+  });
+  const { userName, loggedIn, messages } = user;
+
+  const [userMessageInput, setUserMessageInput] = useState("");
+  const [userNameInput, setUserName] = useState("");
+
+  client.onopen = () => {
+    console.log("Websocket Client Connected");
+  };
+  client.onmessage = (message) => {
+    const dataFromServer = JSON.parse(message.data);
+    // console.log("Got Reply", dataFromServer);
+    if (dataFromServer.type === "message") {
+      setUser({
+        ...user,
+        messages: [...messages,dataFromServer],
+      });
+    }
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    client.send(
+      JSON.stringify({
+        type: "message",
+        msg: userMessageInput,
+        user: userName,
+      })
+    );
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setUser({
+      userName: userNameInput,
+      loggedIn: true,
+      messages: [...messages]
+    });
+  };
+  const handleLoginInput = (e) => {
+    setUserName(e.target.value);
+  };
+  const handleUserMessageChange = (e) => {
+    setUserMessageInput(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loggedIn ? (
+        <div>
+          {messages.map( message => <p>{message.msg}</p>)}
+          <form onSubmit={(e) => handleClick(e)}>
+            <input
+              value={userMessageInput}
+              onChange={(e) => handleUserMessageChange(e)}
+            ></input>
+            <button>Send</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <form onSubmit={(e) => handleLogin(e)}>
+            <input
+              value={userNameInput}
+              onChange={(e) => handleLoginInput(e)}
+            ></input>
+            <button>Login</button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
